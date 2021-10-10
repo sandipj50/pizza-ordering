b72a83a3eddffe0c9742332cfb2003e0344e33e7
Sandip Jha <sandipjha50@gmail.com>
2021-10-11 00:37:20 +0530

    Folder setup done

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const port = process.env.PORT || 8000;
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+//Set template engine
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'resources/views'));
+app.set('view engine','ejs');
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+
+});
+
