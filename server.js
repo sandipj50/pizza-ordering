b0896be24a6810148cc49b54cc20cb2b65a8e1fc
Sandip Jha <sandipjha50@gmail.com>
2021-10-18 01:21:44 +0530

    Added models, sessions,cookies

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const port = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
 
+//Database connection
+const url = 'mongodb://localhost:27017/pizza';
+mongoose.connect(url, { useNewUrlParser:true,useUnifiedTopology:true});
+const connection = mongoose.connection;
 
+connection.on('error',console.error.bind(console, "connection error: "));
+connection.once('open',()=>{
+    console.log('Database connected...');
+})
+//Session store
+let mongoStore = MongoDbStore.create({
+    mongoUrl : url,
+})
+//Session config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave : false,
+    saveUninitialized :false,
+    store:mongoStore,
+    cookie:{
+        maxAge: 1000*60*60*24 //24 hours
+    }
+}))
+
+app.use(flash());
+
+//Assests
 app.use(express.static('public'));
 
 
@@ -14,20 +45,9 @@ app.use(expressLayout);
 app.set('views',path.join(__dirname,'resources/views'));
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    res.render('home');
-})
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
+require('./routes/web')(app);
 
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
 
-})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
